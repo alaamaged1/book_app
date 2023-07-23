30bff6ceb65b18e952fa473b8abe40b5499237da
Alaa <alaamageed99@gmail.com>
2023-07-24 01:33:16 +0200

    create-app

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'Features/splash/presentation/splash_view.dart';
+
+void main() {
+  runApp(const BookApp());
+}
+
+class BookApp extends StatelessWidget {
+  const BookApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: SplashViewScreen(),
+    );
+  }
+}
